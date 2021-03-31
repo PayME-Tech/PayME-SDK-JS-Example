13fe9ba8bc7e0ea4826eb41a59295b98c3a61b23
dattm97 <dattm@payme.vn>
2021-03-31 18:14:19 +0700

    Update hàm pay

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import { useRef } from 'react';
 import './App.css';
 import {WebPaymeSDK} from './WebPayME';
 
+const appSandbox = {
+  publicKey: '-----BEGIN PUBLIC KEY-----\n'
+    + 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAMyTFdiYBiaSIBgqFdxSgzk5LYXKocgT\n'
+    + 'MCx/g1gz9k2jadJ1PDohCs7N65+dh/0dTbT8CIvXrrlAgQT1zitpMPECAwEAAQ==\n'
+    + '-----END PUBLIC KEY-----',
+  privateKey: '-----BEGIN RSA PRIVATE KEY-----\n'
+    + 'MIIBOQIBAAJAZCKupmrF4laDA7mzlQoxSYlQApMzY7EtyAvSZhJs1NeW5dyoc0XL\n'
+    + 'yM+/Uxuh1bAWgcMLh3/0Tl1J7udJGTWdkQIDAQABAkAjzvM9t7kD84PudR3vEjIF\n'
+    + '5gCiqxkZcWa5vuCCd9xLUEkdxyvcaLWZEqAjCmF0V3tygvg8EVgZvdD0apgngmAB\n'
+    + 'AiEAvTF57hIp2hkf7WJnueuZNY4zhxn7QNi3CQlGwrjOqRECIQCHfqO53A5rvxCA\n'
+    + 'ILzx7yXHzk6wnMcGnkNu4b5GH8usgQIhAKwv4WbZRRnoD/S+wOSnFfN2DlOBQ/jK\n'
+    + 'xBsHRE1oYT3hAiBSfLx8OAXnfogzGLsupqLfgy/QwYFA/DSdWn0V/+FlAQIgEUXd\n'
+    + 'A8pNN3/HewlpwTGfoNE8zCupzYQrYZ3ld8XPGeQ=\n'
+    + '-----END RSA PRIVATE KEY-----'
+}
+
 const configs = {
-  connectToken:
-    "bteBmKpT3Hdwr27ygseRoDZTsb1sLyBTCyz1uO5nOavSncaTAlGwXfnJRlIUf4L94VfKQUgDHUr1vcvZPMXYA3elhC+DomnoJliGhWCC17I=",
-  appToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6Njg2OH0.JyIdhQEX_Lx9CXRH4iHM8DqamLrMQJk5rhbslNW4GzY",
+  connectToken: 'iqQQ9STvIfMtzlppuHVJw0naE5vRfxGpxuL/U8p2TxmFWWn9ZU597BR1O9bgAzv+Ra/ineoq5j4l/3xbjitzMqFBPPPauqT+RLEM4N5eJb8=',
+  appToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6MTQsImlhdCI6MTYxNDE2NDI3MH0.MmzNL81YTx8XyTu6SczAqZtnCA_ALsn9GHsJGBKJSIk',
+
+  // connectToken:
+  //   "bteBmKpT3Hdwr27ygseRoDZTsb1sLyBTCyz1uO5nOavSncaTAlGwXfnJRlIUf4L94VfKQUgDHUr1vcvZPMXYA3elhC+DomnoJliGhWCC17I=",
+  // appToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6Njg2OH0.JyIdhQEX_Lx9CXRH4iHM8DqamLrMQJk5rhbslNW4GzY",
   clientId: "0f816b09bdb040e8",
-  env: "dev",
+  env: "sandbox",
   partner: {
     type: "web",
     paddingTop: 20,
@@ -17,14 +36,21 @@ const configs = {
   },
   // showLog: "1",
   configColor: ["#00ffff", "#ff0000"],
-  // publicKey: appSandbox.publicKey,
-  // privateKey: appSandbox.privateKey,
-  // xApi: '14'
-  xApi: "6868",
+  publicKey: appSandbox.publicKey,
+  privateKey: appSandbox.privateKey,
+  xApi: '14',
+  // xApi: "6868",
 }
 
 function App() {
   const refPaymeSDK = useRef(null)
+
+  const login = () => {
+    refPaymeSDK.current?.login(data => {
+      console.log("==========================object", data)
+      alert(JSON.stringify(data))
+    })
+  }
 
   const openWallet = () => {
     refPaymeSDK.current?.openWallet()
@@ -40,8 +66,13 @@ function App() {
   const deposit = (param) => {
     refPaymeSDK.current?.deposit(param)
   }
+
   const withdraw = (param) => {
     refPaymeSDK.current?.withdraw(param)
+  }
+
+  const pay = (param) => {
+    refPaymeSDK.current?.pay(param)
   }
 
   const getListService = () => {
@@ -77,6 +108,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div style={{display: 'flex', flexDirection: 'column'}}>
+          <button style={{marginBottom: 12}} type="button" onClick={() => login()}>Login</button>
+
           <button style={{marginBottom: 12}} type="button" onClick={() => openWallet()}>Open Wallet</button>
 
           <button style={{marginBottom: 12}} type="button" onClick={() => deposit(10000, "desc")}>Deposit</button>
@@ -92,6 +125,8 @@ function App() {
           <button style={{marginBottom: 12}} type="button" onClick={() => openService()}>Open Service (Học phí)</button>
 
           <button style={{marginBottom: 12}} type="button" onClick={() => getListPaymentMethod()}>Get List Payment Method</button>
+
+          <button style={{marginBottom: 12}} type="button" onClick={() => pay({amount: 2000000, orderId: Date.now().toString(), storeId: 24088141, note: 'note'})}>Pay</button>
 
         </div>
 
--- src/WebPayME.js
+++ src/WebPayME.js
@@ -1,23 +1,61 @@
 import { Component } from 'react';
-// import {PaymeWebSdk} from './PaymeSDK'
+import {PaymeWebSdk} from './PaymeSDK'
 
 export class WebPaymeSDK extends Component {
   constructor(props) {
       super(props)
       this.state = {
-        iframeVisible: { state: false, hidden: false }
+        iframeVisible: { state: false, hidden: false } // Biến dùng để bật tắt iFreame
       }
       this.configs = props.configs
+      this.isLogin = false
       // eslint-disable-next-line no-undef
       this._webPaymeSDK = new PaymeWebSdk(props.configs, {id: 'paymeId'});
   }
 
+  _checkActiveAndKyc = () => {
+    if (this.configs?.accountStatus !== 'KYC_OK') {
+      alert(this.configs?.accountStatus)
+      return false
+    }
+    return true
+  }
+
+  login = (callback) => {
+    this.setState({
+      iframeVisible: { state: true, hidden: true },
+    })
+
+    this._webPaymeSDK.login()
+    .then(res => {
+      if (res) {
+        const newConfigs = {
+          ...this.configs,
+          ...res
+        }
+        this.configs = newConfigs
+        this._webPaymeSDK = new PaymeWebSdk(newConfigs, {id: 'paymeId'})
+        this.isLogin = true
+      }
+      console.log('ressss', res)
+      callback(res)
+    })
+    .catch(err => console.log(err))
+  }
+
+
   openWallet = () => {
+    if (!this.isLogin) {
+      alert('NOT LOGIN')
+      return
+    }
+
     this.setState({
       iframeVisible: { state: true, hidden: false },
     })
 
     this._webPaymeSDK.openWallet().then(res => {
+      console.log('ressss', res)
       if (res.type === 'onClose') {
         this.setState({
           iframeVisible: { state: false, hidden: false },
@@ -27,6 +65,15 @@ export class WebPaymeSDK extends Component {
   }
 
   deposit = (param) => {
+    if (!this.isLogin) {
+      alert('NOT LOGIN')
+      return
+    }
+
+    if (!this._checkActiveAndKyc()) {
+      return
+    }
+
     this.setState({
       iframeVisible: { state: true, hidden: false },
     })
@@ -41,6 +88,15 @@ export class WebPaymeSDK extends Component {
   }
 
   withdraw = (param) => {
+    if (!this.isLogin) {
+      alert('NOT LOGIN')
+      return
+    }
+
+    if (!this._checkActiveAndKyc()) {
+      return
+    }
+
     this.setState({
       iframeVisible: { state: true, hidden: false },
     })
@@ -54,7 +110,35 @@ export class WebPaymeSDK extends Component {
     }).catch(err => console.log(err))
   }
 
+  pay = (param) => {
+    if (!this.isLogin) {
+      alert('NOT LOGIN')
+      return
+    }
+
+    if (!this._checkActiveAndKyc()) {
+      return
+    }
+
+    this.setState({
+      iframeVisible: { state: true, hidden: false },
+    })
+    
+    this._webPaymeSDK.pay(param).then(res => {
+      if (res.type === 'onClose') {
+        this.setState({
+          iframeVisible: { state: false, hidden: false },
+        })
+      }
+    }).catch(err => console.log(err))
+  }
+
   getBalance = (callback) => {
+    if (!this.isLogin) {
+      alert('NOT LOGIN')
+      return
+    }
+
     this.setState({
       iframeVisible: { state: true, hidden: true },
     })
@@ -63,6 +147,11 @@ export class WebPaymeSDK extends Component {
   }
 
   getListService = (callback) => {
+    if (!this.isLogin) {
+      alert('NOT LOGIN')
+      return
+    }
+
     this.setState({
       iframeVisible: { state: true, hidden: true },
     })
@@ -71,6 +160,11 @@ export class WebPaymeSDK extends Component {
   }
 
   getAccountInfo = (callback) => {
+    if (!this.isLogin) {
+      alert('NOT LOGIN')
+      return
+    }
+
     this.setState({
       iframeVisible: { state: true, hidden: true },
     })
@@ -79,6 +173,15 @@ export class WebPaymeSDK extends Component {
   }
 
   openService = () => {
+    if (!this.isLogin) {
+      alert('NOT LOGIN')
+      return
+    }
+
+    if (!this._checkActiveAndKyc()) {
+      return
+    }
+
     this.setState({
       iframeVisible: { state: true, hidden: false },
     })
@@ -93,6 +196,11 @@ export class WebPaymeSDK extends Component {
   }
 
   getListPaymentMethod = (callback) => {
+    if (!this.isLogin) {
+      alert('NOT LOGIN')
+      return
+    }
+
     this.setState({
       iframeVisible: { state: true, hidden: true },
     })
